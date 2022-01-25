55c2b5e88f1671738a658189b794ad03e4f9ba14
masudurrahman <masudurrtr01@gmail.com>
2022-01-25 12:43:47 +0600

    updated the files

--- assets/functions.js
+++ assets/functions.js
@@ -8,11 +8,15 @@ function zero(time) {
 function sendData(key, arr) {
     let data = JSON.stringify(arr)
     localStorage.setItem(key, data);
-    return true;
+    
 }
-// Data get from local storage
-function getData(key) {
-    let data = localStorage.getItem(key)
 
-    return JSON.parse(data);
+// Data get from local storage
+
+function getData(key) {
+
+    let data = localStorage.getItem(key)
+    
+    return data ? JSON.parse(data) : false;
+    
 }
--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,34 @@
 //  Age Calculator start here
-const sign_up_form = document.getElementById('sign_up_form');
-const output = document.getElementById('output');
+const sign_up_form = document.getElementById('sign_up_form')
+const output = document.getElementById('output')
 const name = document.getElementById('name')
 
 sign_up_form.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    let date = this.querySelector('input[type="date"]').value;
-    let time = this.querySelector('input[type="time"]').value;
+    let date = this.querySelector('input[type="date"]').value
+    let time = this.querySelector('input[type="time"]').value
       
     if (name.value == '' || date.value == '' || time.value == '') {
         output.innerHTML = `<p class = 'alert alert-danger'>All fields are required</p>`
     } else{
+    
         let start_time = new Date()
-    let end_time = new Date(date + ' ' + time);
+    let end_time = new Date(date + ' ' + time)
         
     let time_diff = Math.floor(Math.abs(end_time.getTime() - start_time.getTime()));
 
-    let total_sec = Math.floor(time_diff / 1000);
-    let total_min = Math.floor(total_sec / 60);
-    let total_hour = Math.floor(total_min / 60);
-    let total_day = Math.floor(total_hour / 24);
+    let total_sec = Math.floor(time_diff / 1000)
+    let total_min = Math.floor(total_sec / 60)
+    let total_hour = Math.floor(total_min / 60)
+    let total_day = Math.floor(total_hour / 24)
         
-    let total_month = Math.floor(total_day / 30);
-    let total_year = Math.floor(total_month / 12);
+    let total_month = Math.floor(total_day / 30.38)
+    let total_year = Math.floor(total_month / 12)
         
       
-    let month = total_month - (total_year * 12)
-    let day = total_day - (total_year * 12 * 30 ) - (month * 30)    
+    let month = Math.floor(Math.abs(total_month - (total_year * 12)));
+    let day = Math.floor(Math.abs(total_day - (total_year * 12 * 30.38) - (month * 30.38)))
 
     
         output.innerHTML = ` <p class= 'alert alert-success'>
@@ -35,62 +36,170 @@ sign_up_form.addEventListener('submit', function (e) {
         
         </p> `
     }
+    
+    this.querySelector('input[type="date"]').value = ''
+    this.querySelector('input[type="time"]').value = ''
+    this.querySelector('#name').value= ''
+
 })
 //  Age Calculator end here
 
+
 // - Product with LS start here
-const product_list = document.getElementById('product_list');
+// get elements for products
+const add_products = document.getElementById('add_products')
+const products_area = document.querySelector('.products_area')
+// products submit form
 
-// get data from local storage
-let products = getData('products');
+add_products.addEventListener('submit', function (e) {
+    e.preventDefault()
+    
+    let name = this.querySelector('input[name="name"]').value
+    let price = this.querySelector('input[name="price"]').value
+    let sale = this.querySelector('input[name="sale"]').value
+    let photo = this.querySelector('input[name="photo"]').value
+    
+    let product_arr
 
-products.map(data => {
-    product_list.innerHTML += `
-    <div class="col-md-3">
-            <div class="card shadow">
-                <img class="card-image" src="${data.photo}" alt="">
-            
-                <div class="card-body">
-                    <h4> ${data.name}</h4>
-                    <p> <span class="regular_price text-danger mr-3">$${data.price}</span><span class="sale_price">$${data.sale}</span></p>
-                    
-                    <hr>
-                    <button class="btn btn-success">Add to cart</button>
-                </div>
-            </div>
-        </div>
-    `
+    if (getData('add_products')) {
+        product_arr = getData('add_products')
+    } else {
+        product_arr = []
+    }
+
+    product_arr.push({
+        name: name,
+        price: price,
+        sale: sale,
+        photo: photo
+    })
+
+    sendData('add_products', product_arr)
+
+    allProducts()
+
+    
+    this.querySelector('input[name="name"]').value = ''
+    this.querySelector('input[name="price"]').value = ''
+    this.querySelector('input[name="sale"]').value = ''
+    this.querySelector('input[name="photo"]').value = ''
+
 })
+
+allProducts()
+function allProducts(){
+
+    let all_products = getData('add_products')
+
+    let data = ''
+
+    all_products.map(p_data => {
+        data += `
+
+        <div class="col-md-3">
+             <div class="card shadow">
+               <img src="${p_data.photo}" alt="">
+             <div class="card-body">
+                <h4>${p_data.name}</h4>
+                 <p> <span class="mr-5 text-danger">${p_data.price}</span><span>${p_data.sale}</span></p>
+               </div>
+               <input class="btn btn-secondary " value="Add to cart">
+                        
+            </div>
+        </div> 
+        
+        `
+
+    })
+
+    products_area.innerHTML = data
+    
+} 
 // - Product with LS end here
 
-//- Team member with LS start here
-const member_list = document.getElementById('member_list');
 
+// - Devs with LS start here
+// get elements for devs
+const devs_form = document.getElementById('devs_form')
+const devs_area = document.querySelector('.devs-area')
 
-// get data from local storage
-let members = getData('members');
+// Devs form submit 
+devs_form.addEventListener('submit', function(e){
+    e.preventDefault()
 
-members.map(data => {
-    member_list.innerHTML += `
-    <div class="col-md-4 col-lg-3">
-            <div class="card shadow">
-                <img class="card-image" src="${data.photo}" alt="">
-            
-                <div class="card-body">
-                    <h4> ${data.name}</h4>
-                    <p class="bg-info text-white p-1">${data.skill}</p>
+    let name = this.querySelector('input[name="name"]')
+    let gender = this.querySelector('input[name="gender"]:checked')
+    let skill = this.querySelectorAll('input[name="skill"]:checked')
+    let photo = this.querySelector('input[name="photo"]') 
+    let email = this.querySelector('input[name="email"]')
+        
+    let skills_arr = []
 
-                    <p> <span class="regular_price text-danger mr-3">$500</span><span class="sale_price">$350</span></p>
-                    
-                    <hr>
-                    <button class="btn btn-success">Add to cart</button>
-                </div>
-            </div>
-    </div>  
-    
-    `
+    for(let i = 0; i < skill.length ; i++){
+        skills_arr.push(skill[i].value)
+    }
+
+    let data_arr
+
+    if(getData('devs')){
+        data_arr = getData('devs')
+    }else{
+        data_arr = []
+    }
+
+    data_arr.push({
+        name : name.value, 
+        gender : gender.value, 
+        skills : skills_arr,
+        photo: photo.value,
+        email: email.value
+    });
+
+    sendData('devs', data_arr)
+
+    allDevs()
+
 })
 
-//- Team member with LS end here
 
+allDevs()
+function allDevs(){
+    let all_devs = getData('devs')
+
+    let data1 = ''
+    all_devs.map(d => {
+
+        let lists = ''
+
+        d.skills.map(list => {
+            lists += '<li> '+ list +'  </li>'
+        });
+
+        data1 += `
+                <div class="col-md-4 my-3">
+						<div class="card shadow">
+							<img src="${ d.photo }" alt="">
+							<div class="card-body">
+								<h2>${ d.name }</h2>
+								<p>Gender : ${ d.gender }</p>
+								
+								<label>Skills : </label>
+								
+								<ul class="list-group">
+                                    
+                                    ${ lists }
+								
+								</ul>
+                                <p id="email">${d.email}</p>
+								
+							</div>
+						</div>
+					</div>  `
+
+    })
+
+    devs_area.innerHTML = data1
+
+}
+// - Devs with LS end here
 
